8f65b1beb930b32c9deec61f36bb2dc7c0375dc5
Darko Kukovec <darko.kukovec@infinum.hr>
2015-10-26 23:30:08 +0100

    code streamlining

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
-var _ = require('lodash');
-
 var init = require('./lib/init');
-var async = require('./lib/async');
-var sync = require('./lib/sync');
+
+var implementedMethods = [
+  'exists',
+  'mkdir',
+  'readFile',
+  'rmdir',
+  'stat',
+  'unlink',
+  'writeFile'
+];
 
 var lib = {
   init: init
 };
 
-_.extend(lib, async, sync);
+implementedMethods.forEach(function(method) {
+  lib[method] = require('./lib/async/' + method);
+  lib[method + 'Sync'] = require('./lib/sync/' + method);
+});
 
 module.exports = Object.freeze(lib);
